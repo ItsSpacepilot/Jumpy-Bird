a8cb6d8721b824ff295524a03a687b8db9d1c191
spacepilot <spacepoilott@gmail.com>
2024-03-11 15:20:02 +0330

    feat: impl pipe spawn function

---
+++ Assets/Scripts/PipeSpawnScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    [SerializeField] private GameObject pipePrefab;
+    [SerializeField] private float spawnRate = 4;
+    [SerializeField] private float hightOffset = 10;
+    private float timer = 0;
+
+    private void Start()
+    {
+        SpawnPipe();
+    }
+    private void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            SpawnPipe();
+            timer = 0;
+        }
+    }
+    private void SpawnPipe()
+    {
+        float highestPoint = transform.position.y + hightOffset;
+        float lowestPoint = transform.position.y - hightOffset;
+
+        Vector3 spawnPosition = new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0);
+        
+
+        Instantiate(pipePrefab, spawnPosition, transform.rotation);
+    }
+}
