7b357ee28cb20b44eed6e9a8a7d6e3a2abd12305
spacepilot <spacepoilott@gmail.com>
2024-03-11 15:19:25 +0330

    feat: impl pipe destroy function
    pipe movment code is now in its own function

--- Assets/Scripts/PipeMovementScript.cs
+++ Assets/Scripts/PipeMovementScript.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class PipeMovementScript : MonoBehaviour
 {
-    [SerializeField] private float speed = 5; 
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float deadZone = -15;
 
     private void Update()
-    {   
+    {
+        MovePipe();
+        DestroyPipe();
+    }
+    private void MovePipe()
+    {
         transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+    private void DestroyPipe()
+    {
+        if (transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+            Debug.Log("Pipe destroyed");
+        }
     }
 }
