4680d8a8b1666e25137b647bfffe62195f1f9976
spacepilot <spacepoilott@gmail.com>
2024-03-16 18:19:04 +0330

    feat: Add gameover function to game

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -7,16 +7,27 @@ public class BirdScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D birdRigidbody;
     [SerializeField] private float flapStrenght = 10;
+    private GameLogicScript logic;
+    private bool isAlive = true;
 
+    private void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<GameLogicScript>();
+    }
     private void Update()
     {
         Flap();
     }
     private void Flap()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             birdRigidbody.velocity = Vector2.up * flapStrenght;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isAlive = false;
+        logic.GameOver();
     }
 }
--- Assets/Scripts/GameLogicScript.cs
+++ Assets/Scripts/GameLogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameLogicScript : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject gameoverScreen;
     private int score = 0;
 
     [ContextMenu("Add SCORE")]
@@ -15,5 +17,12 @@ public class GameLogicScript : MonoBehaviour
         score += scoreToAdd;
         scoreText.text = $"{score}";
     }
-    
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void GameOver()
+    {
+        gameoverScreen.SetActive(true);
+    }
 }
