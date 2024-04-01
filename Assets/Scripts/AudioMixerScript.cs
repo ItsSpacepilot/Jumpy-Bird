92596b9742ac723bfab94dda40855685691473bb
spacepilot <spacepoilott@gmail.com>
2024-04-01 19:32:37 +0330

    feat: Add Volume Setting menu

--- Assets/Scripts/AudioMixerScript.cs
+++ Assets/Scripts/AudioMixerScript.cs
@@ -9,10 +9,20 @@ public class AudioMixerScript : MonoBehaviour
     [SerializeField] AudioMixer gameAudiomixer;
 
     private const string MASTER_VOLUME = "Master Volume";
+    private const string SOUNDEFFECT_VOLUME = "SoundEffect Volume";
+    private const string SOUNDTRACK_VOLUME = "SoundTrack Volume";
 
     public void ChangeMasterVolume(float Volume)
     {
         gameAudiomixer.SetFloat(MASTER_VOLUME, math.log10(Volume) * 20f);
+    }
+    public void ChangeSoundEffectVolume(float Volume)
+    {
+        gameAudiomixer.SetFloat(SOUNDEFFECT_VOLUME, math.log10(Volume) * 20f);
+    }
+    public void ChangeSoundTrackVolume(float Volume)
+    {
+        gameAudiomixer.SetFloat(SOUNDTRACK_VOLUME, math.log10(Volume) * 20f);
     }
 
 }
